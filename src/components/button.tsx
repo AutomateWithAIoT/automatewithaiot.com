75b5be1285d70b0831677cf80cbf759c8c45dbcd
dev <dev@automatewithaiot.com>
2025-04-21 10:12:35 +0530

    updated the homepage

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -13,7 +13,7 @@ export const Button = component$<ButtonProps>((props) => {
         "z-20 cursor-pointer rounded-full border-2 px-12 py-4 transition-colors duration-300 " +
         (props.theme === "dark"
           ? "border-emerald-950 bg-emerald-950 text-neutral-50 hover:bg-neutral-50 hover:text-emerald-950"
-          : "border-neutral-50 bg-neutral-50 text-emerald-950 hover:border-emerald-950 hover:bg-emerald-950 hover:text-neutral-50")
+          : "border-neutral-50 bg-white text-emerald-950 hover:border-emerald-950 hover:bg-emerald-950 hover:text-neutral-50")
       }
       onClick$={() => {
         if (props.link) {
