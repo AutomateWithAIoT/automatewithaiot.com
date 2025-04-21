0ec595578b3adab66e1a50bf6861357bee5b1a74
dev <dev@automatewithaiot.com>
2025-04-21 16:14:09 +0530

    Authentication is mostly complete

--- src/routes/app/dashboard/layout.tsx
+++ src/routes/app/dashboard/layout.tsx
@@ -1,14 +1,22 @@
-import { component$, Slot } from "@builder.io/qwik";
+import { $, component$, Slot, useVisibleTask$ } from "@builder.io/qwik";
 import { Sidebar } from "~/components/dashboard/sidebar/sidebar";
 import { Navbar } from "~/components/dashboard/navbar/navbar";
 
 export default component$(() => {
+  useVisibleTask$(
+    $(() => {
+      const isLoggedIn = sessionStorage.getItem("isLoggedIn");
+      if (!isLoggedIn) {
+        location.href = "/app/auth/Login";
+      }
+    }),
+  );
   return (
-    <div class='flex h-screen bg-emerald-50 dark:bg-gray-900'>
+    <div class="flex h-screen bg-emerald-50 dark:bg-gray-900">
       <Sidebar />
-      <div class='flex flex-col flex-1 overflow-hidden'>
+      <div class="flex flex-1 flex-col overflow-hidden">
         <Navbar />
-        <main class='flex-1 overflow-y-auto p-4 md:p-6'>
+        <main class="flex-1 overflow-y-auto p-4 md:p-6">
           <Slot />
         </main>
       </div>
