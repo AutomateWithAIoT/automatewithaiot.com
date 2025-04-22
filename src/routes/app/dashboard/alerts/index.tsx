933ff3781d3b1fcfada5d91c57460e129114dc6b
dev <dev@automatewithaiot.com>
2025-04-22 09:14:40 +0530

    Fixes on dashboard

--- src/routes/app/dashboard/alerts/index.tsx
+++ src/routes/app/dashboard/alerts/index.tsx
@@ -6,7 +6,7 @@ export default component$(() => {
   const alerts = useSignal([...mockAlerts])
   const filter = useSignal("all")
 
-  const clearAlert = $((alertId) => {
+  const clearAlert = $((alertId:string) => {
     alerts.value = alerts.value.filter((alert) => alert.id !== alertId)
   })
 
@@ -16,7 +16,7 @@ export default component$(() => {
 
   const filteredAlerts = useSignal([...mockAlerts])
 
-  const updateFilter = $((newFilter) => {
+  const updateFilter = $((newFilter:string) => {
     filter.value = newFilter
     if (newFilter === "all") {
       filteredAlerts.value = [...alerts.value]
@@ -85,9 +85,9 @@ export default component$(() => {
             stroke="currentColor"
           >
             <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
+              stroke-Linecap="round"
+              stroke-Linejoin="round"
+              stroke-Width="2"
               d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
             />
           </svg>
