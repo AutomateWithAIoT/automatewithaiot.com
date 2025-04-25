a21444304a25254abbff033de95632494daa962e
dev <dev@automatewithaiot.com>
2025-04-25 12:59:52 +0530

    Blogs updated with reset password UI

--- src/routes/app/auth/layout.tsx
+++ src/routes/app/auth/layout.tsx
@@ -11,7 +11,7 @@ export default component$(() => {
           <h4 class="mb-4 text-center text-2xl font-bold">
             Welcome to Smart Home Solution
           </h4>
-          <p class="mt-4">
+          <p class="mt-4 text-center">
             Experience the future of home automation with AI-powered features.
           </p>
         </div>
